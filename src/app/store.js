1b1f542252fafb555d4362b743c7ac7e9a5889e5
harsh bhama <harshbhama96@gmail.com>
2019-04-10 14:06:41 +0530

    add promise

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux"
 import mathReducer from "./reducers/mathReducer"
 import userReducer from "./reducers/userReducer"
 
+
 import thunk from "redux-thunk"
+
+
+import promise from "redux-promise-middleware"
 
 const myLogger = (store) => (next) => (action) => {
     console.log("Logged in", action)
@@ -14,7 +18,7 @@ export default createStore(
         mathReducer, 
         userReducer}),
      {}, 
-     applyMiddleware(thunk))
+     applyMiddleware(thunk), promise())
 
      
       
